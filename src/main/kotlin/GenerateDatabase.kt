c04e1f0fcb6bbd24bf1648dacd4b35cbb7fe4681
kacienk <ktcienkosz@gmail.com>
2024-01-17 01:11:04 +0100

    add many to many relations handling

--- src/main/kotlin/GenerateDatabase.kt
+++ src/main/kotlin/GenerateDatabase.kt
@@ -1,14 +1,9 @@
 import orm.SqlTypeMapper
-import orm.decorators.Column
+import orm.decorators.*
 import kotlin.reflect.KClass
-import kotlin.reflect.full.findAnnotation
-import kotlin.reflect.full.memberProperties
-import orm.decorators.Entity
-import orm.decorators.JoinColumn
-import orm.decorators.PrimaryKey
 import java.util.*
 import kotlin.reflect.KProperty1
-import kotlin.reflect.full.declaredMemberProperties
+import kotlin.reflect.full.*
 
 fun main(args: Array<String>) {
     if (args.size != 1) {
@@ -52,6 +47,8 @@ fun generateCreateSql(entityClasses: List<KClass<*>>): String {
 
     val tableDefinitions = generateSqlTableDefinitions(entityClasses)
     createSql.append(tableDefinitions)
+    val manyToManyTableDefinitions = generateSqlForManyToManyTableDefinitions(entityClasses)
+    createSql.append(manyToManyTableDefinitions)
     val relationDefinitions = generateSqlRelationDefinitions(entityClasses)
     createSql.append(relationDefinitions)
 
@@ -94,12 +91,12 @@ fun generateSqlForProp(prop: KProperty1<out Any, *>): String {
     var nullInfo = ""
     var propName = ""
     if (joinColumnAnnotation != null) {
-        nullInfo = if (joinColumnAnnotation.nullable) "" else " is not null"
+        nullInfo = if (joinColumnAnnotation.nullable) "" else " not null"
         propName = joinColumnAnnotation.name.ifEmpty { prop.name }
     }
     else if (columnAnnotation != null)
     {
-        nullInfo = if (columnAnnotation.nullable) "" else " is not null"
+        nullInfo = if (columnAnnotation.nullable) "" else " not null"
         propName = columnAnnotation.name.ifEmpty { prop.name }
     }
     if (propName.isEmpty()) throw IllegalArgumentException("There is no column name")
@@ -114,6 +111,92 @@ fun generateSqlForProp(prop: KProperty1<out Any, *>): String {
 fun generateSqlForPrimaryKey(entityClass: KClass<*>): String {
     val primaryKeyName = getPrimaryKeyName(entityClass)
     return "  primary key ($primaryKeyName)\n"
+}
+
+fun generateSqlForManyToManyTableDefinitions(entityClasses: List<KClass<*>>): String {
+    val manyToManyTableDefinitions = StringBuilder()
+
+    val listOfRelatedEntities = getListOfSetsForManyToManyRelations(entityClasses)
+    for (relatedEntities in listOfRelatedEntities) {
+        val manyToManyTableSql = generateSqlForManyToManyTable(relatedEntities)
+        manyToManyTableDefinitions.append(manyToManyTableSql)
+    }
+
+    return manyToManyTableDefinitions.toString()
+}
+
+fun getListOfSetsForManyToManyRelations(entityClasses: List<KClass<*>>): List<Set<KClass<*>>> {
+    val listOfRelations: MutableList<Set<KClass<*>>> = mutableListOf()
+
+    for (entityClass in entityClasses) {
+        val propsWithManyToMany = entityClass.declaredMemberProperties.filter { prop ->
+            prop.annotations.any { it is ManyToMany }
+        }
+
+        for (prop in propsWithManyToMany)
+        {
+            val manyToManyRelationSet = getManyToManyRelationSet(entityClass, prop)
+            if (!listOfRelations.contains(manyToManyRelationSet))
+                listOfRelations.add(manyToManyRelationSet)
+        }
+    }
+
+    return listOfRelations
+}
+
+fun getManyToManyRelationSet(entityClass: KClass<*>, prop: KProperty1<out Any, *>): Set<KClass<*>> {
+    if (!prop.returnType.isSubtypeOf(Collection::class.createType()))
+        throw IllegalArgumentException("Field marked with @ManyToMany must be of type Collection")
+    val propClassTypeArgument = prop.returnType.arguments.firstOrNull()
+        ?: throw IllegalArgumentException("Type of the field marked with @ManyToMany must have an argument")
+    val relatedFieldType = propClassTypeArgument.type
+        ?: throw  IllegalArgumentException(
+            "Type of the field marked with @ManyToMany cannot have a star projection argument"
+        )
+    val relatedFieldClass = relatedFieldType.classifier as KClass<*>
+    relatedFieldClass.findAnnotation<Entity>() ?: throw  IllegalArgumentException(
+        "Type of the field marked with @ManyToMany must be annotated with @Entity"
+    )
+    relatedFieldClass.declaredMemberProperties.find { relatedClassProp ->
+        relatedClassProp.annotations.any { it is ManyToMany }
+                && relatedClassProp.returnType.isSubtypeOf(Collection::class.createType())
+                && relatedClassProp.returnType.arguments.firstOrNull()?.type?.classifier as KClass<*> == entityClass
+    } ?: throw IllegalArgumentException(
+        "Related field $relatedFieldClass does not have proper corresponding field marked with @ManyToMany"
+    )
+
+    return setOf(entityClass, relatedFieldClass)
+}
+
+fun generateSqlForManyToManyTable(relatedEntities: Set<KClass<*>>): String {
+    val manyToManyTable = StringBuilder()
+
+    val firstTable = relatedEntities.first()
+    val secondTable = relatedEntities.last()
+    val firstTableEntity = firstTable.findAnnotation<Entity>()
+        ?: throw IllegalArgumentException("Type of the field marked with @ManyToMany must be marked with @Entity")
+    val secondTableEntity = secondTable.findAnnotation<Entity>()
+        ?: throw IllegalArgumentException("Type of the field marked with @ManyToMany must be marked with @Entity")
+    val firstTableName = firstTableEntity.tableName.ifEmpty { firstTable.simpleName }
+    val secondTableName = secondTableEntity.tableName.ifEmpty { secondTable.simpleName }
+    manyToManyTable.append("create table ${firstTableName}_$secondTableName (\n")
+
+    val firstTablePrimaryKeyName = getPrimaryKeyName(firstTable)
+    val secondTablePrimaryKeyName = getPrimaryKeyName(secondTable)
+    manyToManyTable.append(
+        "  primary key (${firstTableName}_$firstTablePrimaryKeyName, ${secondTableName}_$secondTablePrimaryKeyName),\n"
+    )
+    manyToManyTable.append(
+        "  foreign key (${firstTableName}_$firstTablePrimaryKeyName)"
+                + " references $firstTableName($firstTablePrimaryKeyName),\n"
+    )
+    manyToManyTable.append(
+        "  foreign key (${secondTableName}_$secondTablePrimaryKeyName)"
+                + " references $secondTableName($secondTablePrimaryKeyName)\n"
+    )
+    manyToManyTable.append(");\n\n")
+
+    return manyToManyTable.toString()
 }
 
 fun generateSqlRelationDefinitions(entityClasses: List<KClass<*>>): String {
