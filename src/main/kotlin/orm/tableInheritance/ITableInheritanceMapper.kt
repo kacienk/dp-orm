da077df0bb2e79f0ebb423446fb72f1d29550d99
kacienk <ktcienkosz@gmail.com>
2024-01-24 21:03:00 +0100

    all things

--- src/main/kotlin/orm/tableInheritance/ITableInheritanceMapper.kt
+++ src/main/kotlin/orm/tableInheritance/ITableInheritanceMapper.kt
@@ -1,9 +1,6 @@
 package orm.tableInheritance
 
-import orm.decorators.Entity
 import kotlin.reflect.KClass
-import kotlin.reflect.full.findAnnotation
-import kotlin.reflect.full.superclasses
 
 interface ITableInheritanceMapper {
     fun insert(entity: Any): Boolean
@@ -11,4 +8,5 @@ interface ITableInheritanceMapper {
     fun update(entity: Any): Boolean
     fun remove(id: Long): Boolean
     fun query(q: String): Any? // for hardcore SQL queries like using WHERE, ORDER BY etc. - who needs them btw?
+//    fun find_without_relations(id: Long, entityClass: KClass<*>): Any?
 }
